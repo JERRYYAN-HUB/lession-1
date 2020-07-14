be860c3509e4d33f692c3baade23f98765f84305
JERRYHUB <x120575481@gmail.com>
2020-07-14 21:25:07 +0800

    init hello.py

---
+++ for-loop.py
@@ -0,0 +1,18 @@
+# while loop 
+n = 1
+sum = 0# 紀錄累加的結果
+while n <=10:
+    print(n)
+    sum+=n
+    n+=1
+print(sum)
+
+# for loop # rnage or list
+for i in [3,5,1]:
+    print(i)
+
+for i in "hello":
+    print(i)
+
+for i in range(5):
+    print(i)
